07824b3e2c93e90181dd222e3f7dc8feef6f9f24
Elena Sorokina <elena-sorokina1975@yandex.ru>
2025-10-20 21:31:20 +0300

    Homework 5-1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,57 @@
-export default class NotificationMessage {
+   function createDivElement(className, message = '') {
+    const element = document.createElement("div");
+    element.classList = className;
+    element.textContent = message;
 
+    return element;
+  }
+
+export default class NotificationMessage {
+    currentNotification = null;
+
+    constructor( message = "",{ duration = 2000, type = 'success'} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.element = createDivElement('notification');
+
+    this.element.classList.add(this.type);
+
+    const timer = createDivElement('timer');
+    this.element.append(timer);
+
+    const innerWrapper = createDivElement('inner-wrapper');
+
+    const notificationHeader = createDivElement('notification-header', this.type);
+    innerWrapper.append(notificationHeader);
+
+    const notificationBody = createDivElement('notification-body', this.message);
+    innerWrapper.append(notificationBody);
+
+    this.element.append(innerWrapper);
+  }
+
+  show( targetElement = document.body) {
+    if (NotificationMessage.currentNotification) {
+      NotificationMessage.currentNotification.destroy();
+    }
+    NotificationMessage.currentNotification = this;
+
+    targetElement.append(this.element);
+    this.timerId = setTimeout(this.destroy.bind(this), this.duration);
+  }
+
+  destroy() {
+    if (NotificationMessage.currentNotification === this) {
+      NotificationMessage.currentNotification = null;
+    }
+    this.remove();
+    clearTimeout(this.timerId);
+  }
+
+  remove() {
+    this.element.remove();
+  }
 }
+
